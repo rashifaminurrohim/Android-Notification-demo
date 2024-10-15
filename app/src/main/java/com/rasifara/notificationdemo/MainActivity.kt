50242ea0f3df06d64b72693e7f96a8f093d394fb
rashifaminurrohim <muhammad.rashif@mhs.itenas.ac.id>
2024-10-15 13:37:42 +0700

    - Mengimplementasikan Notifikasi Inbox Style untuk menampilkan beberapa pesan dalam notifikasi. - Menambahkan penanganan izin POST_NOTIFICATIONS untuk Android 13+. - Menginisialisasi NotificationManagerCompat untuk mengelola notifikasi. - Membuat kelas NotificationUtil untuk mengatur Notification Channel pada API 26+. - Menambahkan tombol untuk memicu Notifikasi Inbox Style di MainActivity.

--- app/src/main/java/com/rasifara/notificationdemo/MainActivity.kt
+++ app/src/main/java/com/rasifara/notificationdemo/MainActivity.kt
@@ -1,20 +1,175 @@
 package com.rasifara.notificationdemo
 
+import android.Manifest
+import android.app.Notification
+import android.app.PendingIntent
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.rasifara.notificationdemo.notification.InboxStyleMockData
+import com.rasifara.notificationdemo.notification.NotificationUtil
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val NOTIFICATION_ID = 888
+    private lateinit var mNotificationManagerCompat: NotificationManagerCompat
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted: Boolean? ->
+        if (!isGranted!!) {
+            Toast.makeText(
+                this,
+                "Unable to display Foreground service notification due to permission decline",
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // TODO Step 9: Initialize the notification Manager.
+        // START
+        mNotificationManagerCompat = NotificationManagerCompat.from(this@MainActivity)
+        // END
+
+        val btnInboxStyleNotification = findViewById<Button>(R.id.btn_inbox_style)
+        btnInboxStyleNotification.setOnClickListener(this@MainActivity)
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+
+            R.id.btn_inbox_style -> {
+                generateInboxStyleNotification()
+                return
+            }
         }
+    }
+
+    // TODO Step 4: Create a function to generate the Inbox Style Notification
+    // START
+    /*
+     * Generates a INBOX_STYLE Notification that supports both phone/tablet and wear. For devices
+     * on API level 16 (4.1.x - Jelly Bean) and after, displays INBOX_STYLE. Otherwise, displays a
+     * basic notification.
+     */
+    private fun generateInboxStyleNotification() {
+        // TODO Step 7: Build a INBOX_STYLE notification as below:
+        // START
+        // Main steps for building a INBOX_STYLE notification:
+        //      0. Get your data
+        //      1. Create/Retrieve Notification Channel for O and beyond devices (26+)
+        //      2. Build the INBOX_STYLE
+        //      3. Set up main Intent for notification
+        //      4. Build and issue the notification
+
+        // 1. Create/Retrieve Notification Channel for O and beyond devices (26+).
+        val notificationChannelId: String =
+            NotificationUtil().createInboxStyleNotificationChannel(this)
+
+        // 2. Build the INBOX_STYLE.
+        val inboxStyle =
+            NotificationCompat.InboxStyle()
+                .setBigContentTitle(InboxStyleMockData.mBigContentTitle)
+                .setSummaryText(InboxStyleMockData.mSummaryText)
+
+        // Add each summary line of the new emails, you can add up to 5.
+        for (summary in InboxStyleMockData.mIndividualEmailSummary()) {
+            inboxStyle.addLine(summary)
+        }
+
+        // 3. Set up main Intent for notification that is the Activity that you want to launch when user tap on notification.
+        val mainIntent = Intent(this, MainActivity::class.java)
+        // Gets a PendingIntent containing the intent.
+        val mainPendingIntent =
+            PendingIntent.getActivity(
+                this,
+                0,
+                mainIntent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+
+        // 4. Build and issue the notification.
+        // Because we want this to be a new notification (not updating a previous notification), we
+        // create a new Builder. However, we don't need to update this notification later, so we
+        // will not need to set a global builder for access to the notification later.
+        val notificationCompatBuilder = NotificationCompat.Builder(
+            applicationContext,
+            notificationChannelId
+        )
+
+        notificationCompatBuilder // INBOX_STYLE sets title and content for API 16+ (4.1 and after) when the
+            // notification is expanded.
+            .setStyle(inboxStyle) // Title for API <16 (4.0 and below) devices and API 16+ (4.1 and after) when the
+            // notification is collapsed.
+            .setContentTitle(InboxStyleMockData.mContentTitle) // Content for API <24 (7.0 and below) devices and API 16+ (4.1 and after) when the
+            // notification is collapsed.
+            .setContentText(InboxStyleMockData.mContentText)
+            .setSmallIcon(R.drawable.ic_active_notification)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(
+                    resources,
+                    R.drawable.ic_person
+                )
+            )
+            .setContentIntent(mainPendingIntent)
+            .setDefaults(NotificationCompat.DEFAULT_ALL) // Set primary color (important for Wear 2.0 Notifications).
+            .setColor(
+                ContextCompat.getColor(
+                    applicationContext,
+                    R.color.purple_500
+                )
+            ) // SIDE NOTE: Auto-bundling is enabled for 4 or more notifications on API 24+ (N+)
+            // devices and all Wear devices. If you have more than one notification and
+            // you prefer a different summary notification, set a group key and create a
+            // summary notification via
+            // .setGroupSummary(true)
+            // .setGroup(GROUP_KEY_YOUR_NAME_HERE)
+            // Sets large number at the right-hand side of the notification for API <24 devices.
+            .setSubText(InboxStyleMockData.mNumberOfNewEmails.toString())
+            .setCategory(Notification.CATEGORY_EMAIL) // Sets priority for 25 and below. For 26 and above, 'priority' is deprecated for
+            // 'importance' which is set in the NotificationChannel. The integers representing
+            // 'priority' are different from 'importance', so make sure you don't mix them.
+            .setPriority(InboxStyleMockData.mPriority) // Sets lock-screen visibility for 25 and below. For 26 and above, lock screen
+            // visibility is set in the NotificationChannel.
+            .setVisibility(InboxStyleMockData.mChannelLockscreenVisibility)
+
+        // If the phone is in "Do not disturb mode, the user will still be notified if
+        // the sender(s) is starred as a favorite.
+        for (name in InboxStyleMockData.mParticipants()) {
+            notificationCompatBuilder.addPerson(name)
+        }
+        val notification = notificationCompatBuilder.build()
+        // END
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (ActivityCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
+            }
+        }
+        mNotificationManagerCompat.notify(NOTIFICATION_ID, notification)
+        // END
     }
 }
---
+++ app/src/main/java/com/rasifara/notificationdemo/notification/NotificationUtil.kt
@@ -0,0 +1,42 @@
+package com.rasifara.notificationdemo.notification
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
+import androidx.core.content.getSystemService
+
+class NotificationUtil {
+
+    fun createInboxStyleNotificationChannel(context: Context) : String {
+
+        // NotificationChannels are required for Notifications on O (API 26) and above.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            val channelId : String = InboxStyleMockData.mChannelId
+            val channelName: CharSequence = InboxStyleMockData.mChannelName
+            val channelDescription: String = InboxStyleMockData.mChannelDescription
+            val channelImportance: Int = InboxStyleMockData.mChannelImportance
+            val channelEnableVibrate: Boolean = InboxStyleMockData.mChannelEnableVibrate
+            val channelLockScreenVisibility: Int = InboxStyleMockData.mChannelLockscreenVisibility
+
+            // Initialize Notification Channel
+            val notificationChannel = NotificationChannel(channelId, channelName, channelImportance)
+            notificationChannel.description = channelDescription
+            notificationChannel.enableVibration(channelEnableVibrate)
+            notificationChannel.lockscreenVisibility = channelLockScreenVisibility
+
+            // Adds NotificationChannel to system. Attempting to create an existing notification
+            // channel with its original values performs no operation, so it's safe to perform the
+            // below sequence.
+            val notificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(notificationChannel)
+            return channelId
+
+        } else {
+            return ""
+        }
+    }
+
+}
